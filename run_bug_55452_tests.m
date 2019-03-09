fd9b32d75e41e83a11fcb82104f92318ca3b6d26
Andrew Janke <floss@apjanke.net>
2019-03-09 14:09:10 -0500

    Expand test to do both fgetl() and textscan()

--- run_bug_55452_tests.m
+++ run_bug_55452_tests.m
@@ -14,7 +14,7 @@ endfunction
 
 function run_fixed_text_encoded_file_test (ex_name, ex_dir)
   fprintf ("Running fixed-text encoded file test %s:\n", ex_name);
-  ref_text = slurp_file_one_line (fullfile (ex_dir, "ref.txt"), "UTF-8");
+  ref_text = read_one_line_utf8 (fullfile (ex_dir, "ref.txt"), "UTF-8");
   fprintf("Reference text: %s (%d chars)\n", ref_text, numel (ref_text));
   enc_files = mydir ([ex_dir "/txt-*.txt"]);
   for i_enc_file = 1:numel (enc_files)
@@ -28,15 +28,23 @@ function run_fixed_text_encoded_file_test (ex_name, ex_dir)
       variant = '';
     endif
     fprintf ("running: %s %s %s\n", ex_name, encoding, variant);
-    decoded_text = slurp_file_one_line (fullfile (ex_dir, enc_file), encoding);
-    fprintf ("  decoded: %s (%d chars)\n", decoded_text, numel (decoded_text));
-    ok = isequal (ref_text, decoded_text);
-    if ok
-      fprintf ("  ok: %s %s %s\n", ex_name, encoding, variant);
-    else
-      fprintf ("  FAIL: %s %s %s\n", ex_name, encoding, variant);      
-    endif
+    fprintf ("  fgetl:\n");
+    decoded_text = read_one_line_fgetl (fullfile (ex_dir, enc_file), encoding);
+    check_line (ex_name, encoding, variant, 'fgetl', ref_text, decoded_text);
+    fprintf ("  textscan:\n");
+    decoded_text = read_one_line_textscan (fullfile (ex_dir, enc_file), encoding);
+    check_line (ex_name, encoding, variant, 'textscan', ref_text, decoded_text);
   endfor
+endfunction
+
+function check_line (ex_name, encoding, variant, technique, ref_text, decoded_text)
+  fprintf ("    decoded: %s (%d chars)\n", decoded_text, numel (decoded_text));
+  ok = isequal (ref_text, decoded_text);
+  if ok
+    fprintf ("    ok: %s %s %s - %s\n", ex_name, encoding, variant, technique);
+  else
+    fprintf ("    FAIL: %s %s %s - %s\n", ex_name, encoding, variant, technique);
+  endif  
 endfunction
 
 function [names, d] = mydir (the_dir_arg)
@@ -47,12 +55,39 @@ function [names, d] = mydir (the_dir_arg)
   d = d(tf);
 endfunction
 
-function out = slurp_file_one_line (file, encoding)
-  [fh, msg] = fopen (file, "r", "native", encoding);
-  if fh < 0
+function fid = my_fopen (file, encoding)
+  [fid, msg] = fopen (file, "r", "native", encoding);
+  if fid < 0
+    error ("Failed opening file for reading: %s: %s", msg, file);
+  endif  
+endfunction
+
+function out = read_one_line_fgetl (file, encoding)
+  fid = my_fopen (file, encoding);
+  out = fgetl (fid);
+  fclose (fid);
+endfunction
+
+function out = read_one_line_textscan (file, encoding)
+  fid = my_fopen (file, encoding);
+  lines = textscan (fid, "%s", "Delimiter","");
+  out = lines{1}{1};
+  out = out(:)';
+  fclose (fid);
+endfunction
+
+function out = read_one_line_utf8 (file)
+  % Do a binary read because no transcoding is necessary or desired here
+  [fid, msg] = fopen (file, "r");
+  if fid < 0
     error ("Failed opening file for reading: %s: %s", msg, file);
   endif
-  out = fgetl (fh);
-  fclose (fh);
-  out = out(:)';
+  bytes = fread (file, Inf, '*char');
+  fclose (fid);
+  txt = bytes(:)';
+  LFs = find (txt == "\n");
+  if !isempty (LFs)
+    txt(LFs(1):end) = [];
+  endif
+  out = txt;
 endfunction
